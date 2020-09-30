7d017445eb61c8e1d73e7af9d45c6af7d633391d
Luis Gustavo Vital <luisgustavo2829@gmail.com>
2020-09-29 22:24:36 -0300

    Teste pelo JUnit na classe Campus com os métodos da classe Ativo implementada

---
+++ src/main/java/br/edu/utfpr/dv/sireata/model/TestCampusTest.java
@@ -0,0 +1,12 @@
+package br.edu.utfpr.dv.sireata.model;
+
+import junit.framework.TestCase;
+
+public class TestCampusTest extends TestCase {
+
+    public void testIsAtivo() {
+    }
+
+    public void testSetAtivo() {
+    }
+}
