501fc007246181a815118cbd5b4850dcb4afdb59
Luis Gustavo Vital <luisgustavo2829@gmail.com>
2020-09-29 22:22:46 -0300

    Exclusão SituaçãoComentario e Teste pelo JUnit na classe Usuario com os métodos da classe Ativo implementada

---
+++ src/main/java/br/edu/utfpr/dv/sireata/model/TestUsuarioTest.java
@@ -0,0 +1,12 @@
+package br.edu.utfpr.dv.sireata.model;
+
+import junit.framework.TestCase;
+
+public class TestUsuarioTest extends TestCase {
+
+    public void testIsAtivo() {
+    }
+
+    public void testSetAtivo() {
+    }
+}
